8b0761d6812489ba53bd911e79aeb0594f489b78
krichards-gr <krichards@gravityresearch.com>
2026-06-17 07:45:33 -0500

    feat: added columns to engagement term reference staging table

--- earnings_call_transforms/models/staging/benchmarking_pipeline/stg_engagement_term_reference.sql
+++ earnings_call_transforms/models/staging/benchmarking_pipeline/stg_engagement_term_reference.sql
@@ -1,7 +1,9 @@
 {{ config(schema='social_media_activity_archive') }} -- Override default schema (dataset assignment) to build in the benchmarking BQ dataset
 
-SELECT key_terms,
+SELECT precedence,
+    key_terms,
     engagement_sub_type,
-    engagement_type
+    engagement_type,
+    pattern_description
 
 FROM {{ source('social_media_activity_archive', 'external_engagement_term_reference')}}
